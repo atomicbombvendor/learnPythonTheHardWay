eb79fcecd9216808475ac7beafbbb215c4ffda9b
Eddie.Li <A>
2018-08-23 20:45:41 +0800

    add comment

--- Practices/uploadFile.py
+++ Practices/uploadFile.py
@@ -3,7 +3,6 @@ import os
 
 from ftplib import FTP
 
-
 ftp_server = 'ftp.morningstar.com'
 ftp_user = 'OnDemandMaster'
 ftp_password = 'OnDemandMasterNew1234'
@@ -18,55 +17,70 @@ root = "Y:/GeDataFeed/GEDF2.0/DailyDelta"
 def upload_file(dir_name):
     print "Start to read folder " + root
     postfix = ['2018-08-06.zip', '2018-08-06.ctrl']
-    result = []
+    file_uploaded = []
 
-    for maindir, subdir, file_name_list in os.walk(dir_name):
-        print "maindir " + maindir + " subdir length " + str(len(subdir))
+    for main_dir, sub_dir, file_name_list in os.walk(dir_name):
+        print "main_dir: " + main_dir + "; subdir length: " + str(len(sub_dir))
         for file_name in file_name_list:
             # print "file name" + file_name
-            absolutePath = os.path.join(maindir, file_name)
+            absolutePath = os.path.join(main_dir, file_name)
             for post_fix in postfix:
                 if post_fix in absolutePath:
                     print "#### File {" + file_name + "} find ####"
-                    result.append(absolutePath)
+                    file_uploaded.append(absolutePath)
                     ftp_up(ftp, ftp_root_path, absolutePath)
                     ftp.cwd(ftp_root_path)
 
-    return result
+    return file_uploaded
 
 
 # Connect FTP
 def ftp_connect():
-    ftp = FTP()
+    ftp_connected = FTP()
     try:
-        ftp.connect(ftp_server, ftp_port, ftp_timeout)
-        ftp.login(ftp_user, ftp_password)
-        print ftp.getwelcome()
+        ftp_connected.connect(ftp_server, ftp_port, ftp_timeout)
+        ftp_connected.login(ftp_user, ftp_password)
+        print ftp_connected.getwelcome()
         print "FTP Login Success"
-        return ftp
+        return ftp_connected
     except ftplib.error_perm:
         print "Error, cannot login."
-        ftp.quit()
+        ftp_connected.quit()
         return None
 
 
 # Upload File to FTP
 # ftp_root: ftp_root path
 # file: file absolute path
-def ftp_up(ftp, ftp_root, file):
-    ftp.cwd(ftp_root)
+def ftp_up(ftp_connected, ftp_root, absolute_file):
+    ftp_connected.cwd(ftp_root)
     # file relative path
-    file_folder = os.path.dirname(file).replace(root, '').replace("\\", "/")
-    file_name = os.path.basename(file)
+    file_folder = os.path.dirname(absolute_file).replace(root, '').replace("\\", "/")
+    file_name = os.path.basename(absolute_file)
     ftp_path = file_folder
-    print "Upload %s to %s" % (file_name, ftp_root_path + ftp_path)
+    print ">>>Upload %s to %s" % (file_name, ftp_root_path + ftp_path)
 
     try:
-        ftp.cwd(ftp_root + ftp_path)
+        ftp_connected.cwd(ftp_root + ftp_path)
     except ftplib.error_perm:
-        print "Path is not exists " + ftp_path
+        print ">>>Path is not exists " + ftp_path
+        make_dirs(ftp_connected, ftp_root, ftp_path)
 
-    ftp.storbinary('STOR ' + file_name, open(file, 'rb'))
+    ftp_connected.storbinary('STOR ' + file_name, open(absolute_file, 'rb'))
+
+
+# ftp_root for example: /AA/BB
+# dir_path need to create folder
+# for example: /A/B/C
+# Create Folder /AA/BB/A/B/C
+def make_dirs(ftp_connected, ftp_root, dir_path):
+    dirs = dir_path.replace("\\", "/").split("/")
+    current_path = ftp_root
+    for dir_created in dirs:
+        try:
+            ftp_connected.cwd(current_path + "/" + dir_created)
+        except ftplib.error_perm:
+            ftp_connected.mkd(current_path + "/" + dir_created)
 
 
 if __name__ == '__main__':
